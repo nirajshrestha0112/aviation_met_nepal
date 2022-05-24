1db8aa88d32cc58fba32eba72a67d557176583c9
Sabin Ranabhat <sabin@Xiaomi.local>
2022-05-24 14:53:53 +0545

    hard coded string moved to relative files

--- lib/constant/urls.dart
+++ lib/constant/urls.dart
@@ -17,6 +17,10 @@ const String satelliteImageDataUrl = '$baseUrl/satellite/';
 const String loginUrl = '$baseUrl/login/login/';
 const String lightingDataUrl = '$baseUrl/lightning/';
 const String privacyPolicyUrl = '$baseUrl/getPrivacy/';
-const String weatherCameraImagesUrl = 'https://hydrology.gov.np/gss/api/socket/airport_display/response';
-const String weatherCameraImagesDetailsUrl = 'https://daq.hydrology.gov.np/idaq-files/';
+const String weatherCameraImagesUrl =
+    'https://hydrology.gov.np/gss/api/socket/airport_display/response';
+const String weatherCameraImagesDetailsUrl =
+    'https://daq.hydrology.gov.np/idaq-files/';
 // const String metaDataDecodedUrl = '$baseUrl/api/getMetarData/decoded/';
+
+const String ftpBaseUrl = 'hydrology.gov.np';
