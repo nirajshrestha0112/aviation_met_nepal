60fdb7b1a6bb2a9d21f7caaa6c49074efac2f6b4
nirajshrestha0112 <nirajshrestha0112@gmail.com>
2024-09-27 00:20:45 +1000

    first commit

---
+++ build/app/generated/source/buildConfig/release/dhm/aviation/aviation_met_nepal/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package dhm.aviation.aviation_met_nepal;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "dhm.aviation.aviation_met_nepal";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "1.0.4";
+}
